ad3b9039019a3ef4ab3b232a4a7f4493b385c030
songa1 <songachillethe1her@gmail.com>
2020-10-29 16:00:09 +0200

    made responsive file more useful by adding more styles to make my main page responsive

--- js/nav-responsive.js
+++ js/nav-responsive.js
@@ -1,16 +1,4 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "150px";
     document.getElementById("main").style.display = "none";
-}
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("main").style.display = "block";
-}
-function hideDiv() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("main").style.display = "block";
-    document.getElementById("li-1").style.display = "none";
 }
