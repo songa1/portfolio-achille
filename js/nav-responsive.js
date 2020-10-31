88b83de9ed216b4de8430000107542e038f3b284
songa1 <songachillethe1her@gmail.com>
2020-10-31 23:04:59 +0200

    made responsive navigation bar

--- js/nav-responsive.js
+++ js/nav-responsive.js
@@ -1,4 +1,16 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width ="150px";
-    document.getElementById("mySidebar").style.display ="absolute";
-}
+
+        function openNav() {
+            document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("mySidebar").style.width = "250px";
+        }
+
+        function hideDiv() {
+            document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("mySidebar").style.width = "0";
+            document.getElementById("main").style.display = "block";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("mySidebar").style.width = "0";
+          }
