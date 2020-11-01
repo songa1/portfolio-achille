0724057f350f38ffe098c86cc1626bb71141814d
songa1 <songachillethe1her@gmail.com>
2020-11-01 15:13:23 +0200

    added responsiveness to profile pages

--- js/profile-sections.js
+++ js/profile-sections.js
@@ -5,6 +5,13 @@ function mainPage(){
     document.getElementById("man-about").style.display = "none";
     document.getElementById("man-project").style.display = "none";
     document.getElementById("man-experience").style.display = "none";
+    // js for titles
+    document.getElementById("main-titles").style.display = "grid";
+    document.getElementById("contact-titles").style.display = "none";
+    document.getElementById("skills-titles").style.display = "none";
+    document.getElementById("about-titles").style.display = "none";
+    document.getElementById("project-titles").style.display = "none";
+    document.getElementById("expert-titles").style.display = "none";
     // talking about background color after click
     document.getElementById("mainpage").style.backgroundColor = "#008073";
     document.getElementById("contactpage").style.backgroundColor = "inherit";
@@ -20,6 +27,13 @@ function skillsPage(){
     document.getElementById("man-about").style.display = "none";
     document.getElementById("man-project").style.display = "none";
     document.getElementById("man-experience").style.display = "none";
+    // js for titles
+    document.getElementById("main-titles").style.display = "none";
+    document.getElementById("contact-titles").style.display = "none";
+    document.getElementById("skills-titles").style.display = "grid";
+    document.getElementById("about-titles").style.display = "none";
+    document.getElementById("project-titles").style.display = "none";
+    document.getElementById("expert-titles").style.display = "none";
     // talking about background color after click
     document.getElementById("mainpage").style.backgroundColor = "inherit";
     document.getElementById("contactpage").style.backgroundColor = "inherit";
@@ -35,6 +49,13 @@ function aboutPage(){
     document.getElementById("man-about").style.display = "grid";
     document.getElementById("man-project").style.display = "none";
     document.getElementById("man-experience").style.display = "none";
+    // js for titles
+    document.getElementById("main-titles").style.display = "none";
+    document.getElementById("contact-titles").style.display = "none";
+    document.getElementById("skills-titles").style.display = "none";
+    document.getElementById("about-titles").style.display = "grid";
+    document.getElementById("project-titles").style.display = "none";
+    document.getElementById("expert-titles").style.display = "none";
     // talking about background color after click
     document.getElementById("mainpage").style.backgroundColor = "inherit";
     document.getElementById("contactpage").style.backgroundColor = "inherit";
@@ -50,6 +71,13 @@ function projectPage(){
     document.getElementById("man-about").style.display = "none";
     document.getElementById("man-project").style.display = "grid";
     document.getElementById("man-experience").style.display = "none";
+    // js for titles
+    document.getElementById("main-titles").style.display = "none";
+    document.getElementById("contact-titles").style.display = "none";
+    document.getElementById("skills-titles").style.display = "none";
+    document.getElementById("about-titles").style.display = "none";
+    document.getElementById("project-titles").style.display = "grid";
+    document.getElementById("expert-titles").style.display = "none";
     // talking about background color after click
     document.getElementById("mainpage").style.backgroundColor = "inherit";
     document.getElementById("contactpage").style.backgroundColor = "inherit";
@@ -65,6 +93,13 @@ function expertPage(){
     document.getElementById("man-about").style.display = "none";
     document.getElementById("man-project").style.display = "none";
     document.getElementById("man-experience").style.display = "grid";
+    // js for titles
+    document.getElementById("main-titles").style.display = "none";
+    document.getElementById("contact-titles").style.display = "none";
+    document.getElementById("skills-titles").style.display = "none";
+    document.getElementById("about-titles").style.display = "none";
+    document.getElementById("project-titles").style.display = "none";
+    document.getElementById("expert-titles").style.display = "grid";
     // talking about background color after click
     document.getElementById("mainpage").style.backgroundColor = "inherit";
     document.getElementById("contactpage").style.backgroundColor = "inherit";
@@ -80,6 +115,13 @@ function contactPage(){
     document.getElementById("man-about").style.display = "none";
     document.getElementById("man-project").style.display = "none";
     document.getElementById("man-experience").style.display = "none";
+    // js for titles
+    document.getElementById("main-titles").style.display = "none";
+    document.getElementById("contact-titles").style.display = "grid";
+    document.getElementById("skills-titles").style.display = "none";
+    document.getElementById("about-titles").style.display = "none";
+    document.getElementById("project-titles").style.display = "none";
+    document.getElementById("expert-titles").style.display = "none";
     // talking about background color after click
     document.getElementById("mainpage").style.backgroundColor = "inherit";
     document.getElementById("contactpage").style.backgroundColor = "#008073";
@@ -87,4 +129,17 @@ function contactPage(){
     document.getElementById("aboutpage").style.backgroundColor = "inherit";
     document.getElementById("projectpage").style.backgroundColor = "inherit";
     document.getElementById("expertpage").style.backgroundColor = "inherit";
+}
+
+
+// js for responsive of additional pages on profile
+
+function openBar() {
+    document.getElementById("mainpage").style.display ="block";
+    document.getElementById("skillpage").style.display ="block";
+    document.getElementById("aboutpage").style.display ="block";
+    document.getElementById("projectpage").style.display ="block";
+    document.getElementById("expertpage").style.display ="block";
+    document.getElementById("contactpage").style.display ="block";
+    document.getElementById("managepage").style.display ="block";
 }
