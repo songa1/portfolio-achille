4ca65e3302ca9b95c82b1e81b9a8800185d64ef3
songa1 <songachillethe1her@gmail.com>
2020-11-01 13:42:35 +0200

    saved changes from responsive-ui

--- js/nav-responsive.js
+++ js/nav-responsive.js
@@ -4,12 +4,6 @@
             document.getElementById("mySidebar").style.width = "250px";
         }
 
-        function hideDiv() {
-            document.getElementById("mySidebar").style.display = "none";
-            document.getElementById("mySidebar").style.width = "0";
-            document.getElementById("main").style.display = "block";
-        }
-
         function closeNav() {
             document.getElementById("mySidebar").style.display = "none";
             document.getElementById("mySidebar").style.width = "0";
