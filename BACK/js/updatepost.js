7edd6c1bc3ff4b0288304f1f60ead57bdabaa80f
songa1 <songachillethe1her@gmail.com>
2020-11-10 16:13:39 +0200

    Well working update feature in CRUD Operations

--- BACK/js/updatepost.js
+++ BACK/js/updatepost.js
@@ -1,7 +1,7 @@
 let id = location.hash.slice(1);
 let updateForm = document.querySelector('#update-post');
-let cancelUpdate = document.querySelector('#cancelUP');
-let updateButton = document.querySelector('#updatepost');
+let cancelUpdate = document.querySelector('#cancelUpdate');
+let updateButton = document.querySelector('#updateButton');
 
 function edit(doc){
     
@@ -17,24 +17,26 @@ function edit(doc){
 db.collection('posts').doc(id).get().then((doc)=>{
     edit(doc.data())
 })
-function cancel(){
-    if(confirm("Your post won't be updated. Are you sure?") == true){
-        
-    }else {
-        return false;
-    }
+let mesaage = document.querySelector('#feedMessage');
+updateButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+if(confirm("Do you want to save changes?") == true){
+    db.collection('posts').doc(id).update({
+        author: updateForm.author.value,
+        title: updateForm.title.value,
+        summary: updateForm.summary.value,
+        date: updateForm.date.value,
+        content: updateForm.content.value
+    }).catch(function(error){
+        console.log(error.message);
+    })
+    mesaage.style.display = "block";
+}else {
+    console.log("Changes couldn't be saved")
 }
-function update(){
-    // if(confirm('Do you want to save changes?') == true){
-        db.collection('posts').doc(id).update({
-            author: updateForm.author.value,
-            title: updateForm.title.value,
-            summary: updateForm.summary.value,
-            date: updateForm.date.value,
-            content: updateForm.content.value
-        })
-        window.location.href = "modify.html";
-//     }else {
-//         return false;
-//     }
+    
+})
+
+function lost(){
+    mesaage.style.display = "none";
 }
