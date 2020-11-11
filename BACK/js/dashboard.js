5f29941fb6f6c4bc5413d269960d73ba4db2f1cf
songa1 <songachillethe1her@gmail.com>
2020-11-11 22:28:10 +0200

    I can now edit and publish my about page on the main page

--- BACK/js/dashboard.js
+++ BACK/js/dashboard.js
@@ -51,4 +51,43 @@ db.collection('landing-page').doc('GvsCpto5XsDyhf0lpMaP').get().then((doc)=>{
     landingPage(doc.data())
 })
 
+// About me management space
+function aboutme(doc){
+    let heading = document.querySelector('#aboutheading');
+    let about = document.querySelector('#aboutcontent');
 
+    heading.textContent = doc.heading;
+    about.textContent = doc.about;
+}
+db.collection('my-about').doc('aboutme').get().then((doc)=>{
+    aboutme(doc.data())
+})
+ 
+function aboutInDash(doc){
+    let aboutHeadingDash = document.querySelector('#aboutHeadingDash');
+    let aboutContentInDash = document.querySelector('#aboutContentDash');
+
+    let editHeading = document.querySelector('#aboutHeadingEdit');
+    let editAbout = document.querySelector('#mainAboutEdit');
+
+    aboutHeadingDash.textContent = doc.heading;
+    aboutContentInDash.textContent = doc.about; 
+
+    editHeading.value = doc.heading;
+    editAbout.value = doc.about;
+}
+
+db.collection('my-about').doc('aboutme').get().then((doc)=>{
+    aboutInDash(doc.data())
+})
+
+
+
+//update about
+function aboutUpdate(){
+    console.log('Done');
+    db.collection('my-about').doc('aboutme').update({
+        heading: aboutheadingEdit.value,
+        about: mainaboutEdit.value
+    })
+}
